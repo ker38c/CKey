2e8c33664b385e888d5d21c93bf8564d7f1af4e7
ker38c <128274907+ker38c@users.noreply.github.com>
2025-01-03 23:57:37 +0900

    Add midi controller thread.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
+import threading
 from gui.MainWindow import MainWindow
 from config.Setting import Setting
+from midi.MidiController import MidiController
 
 def main():
     setting = Setting()
     window = MainWindow(setting)
+    midi = MidiController(window.piano_tab.keyboard)
+
+    if midi.connect():
+        # MIDI thread
+        midi_thread = threading.Thread(target=midi.receive)
+        midi_thread.start()
+
+    else:
+        print("midi device is not connected.")
+
+    # gui
     window.start()
+
+    if midi.start:
+        midi.start = False
+        midi_thread.join()
+    print("CKey exit")
 
 
 if __name__ == "__main__":
