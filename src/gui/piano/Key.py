001cb5b2810e7a8935b611e11852bca70c8e1978
ker38c <128274907+ker38c@users.noreply.github.com>
2025-12-14 23:12:06 +0900

    Refactor to separate Keyboard components into individual files.

---
+++ src/gui/piano/Key.py
@@ -0,0 +1,32 @@
+import tkinter
+from config.Setting import Setting
+
+
+class Key(tkinter.Button):
+    def __init__(self, master=None, name: str="", setting: Setting=None, midi=None, **kargs):
+        super().__init__(master=master, **kargs)
+        self.config(activebackground=setting.gui.KeyPushedColor)
+        self.name = name
+        self.midi = midi
+        self.bind('<Button-1>', self.on_press)
+        self.bind('<ButtonRelease-1>', self.on_release)
+
+    def on_press(self, event):
+        if self.midi:
+            self.midi.add_key_event(self.name, True)
+
+    def on_release(self, event):
+        if self.midi:
+            self.midi.add_key_event(self.name, False)
+
+
+class WhiteKey(Key):
+    def __init__(self, master=None, name: str="", setting: Setting=None, midi=None, **kargs):
+        super().__init__(master=master, name=name, setting=setting, midi=midi, **kargs)
+        self.config(background="white")
+
+
+class BlackKey(Key):
+    def __init__(self, master=None, name: str="", setting: Setting=None, midi=None, **kargs):
+        super().__init__(master=master, name=name, setting=setting, midi=midi, **kargs)
+        self.config(background="black")
